3c6098bed7974589239abf34ceec1e26e815dc12
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-20 12:30:34 -0500

    Created Student class from TDD

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+    @Test
+    public void testIfNameIsString(){
+        Student studentName = new Student();
+//        assertEquals("", Student.name);
+
+
+
+    }
+}
